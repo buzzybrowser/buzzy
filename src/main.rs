3cffa8247c8d7f523ebc10dbdb478f3f0464ae02
Michael Mc Donnell <michael@mcdonnell.dk>
2021-04-25 09:54:55 -0700

    Add bevy game engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use bevy::prelude::*;
+
+fn hello_world() {
     println!("Hello, world!");
+}
+
+fn main() {
+    App::build()
+        .add_system(hello_world.system())
+        .run();
 }
